1fe7451cf0be2b5974743c70e550ca836df6ba15
pangwoo96 <junggw96@gmail.com>
2024-10-01 15:51:23 +0900

    feature: User Repository 추가

---
+++ src/main/java/com/sparklenote/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sparklenote.domain.repository;
+
+import com.sparklenote.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
