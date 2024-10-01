65c5a2cb99f51f081a8aee4587bc63b5466e4e74
pangwoo96 <junggw96@gmail.com>
2024-10-01 15:51:13 +0900

    feature: Roll Repository 추가

---
+++ src/main/java/com/sparklenote/domain/repository/RollRepository.java
@@ -0,0 +1,10 @@
+package com.sparklenote.domain.repository;
+
+import com.sparklenote.domain.entity.Roll;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RollRepository extends JpaRepository<Roll, Long> {
+
+}
