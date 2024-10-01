1658006e7741843c333e8bb89c8426637bae21c6
pangwoo96 <junggw96@gmail.com>
2024-10-01 15:50:18 +0900

    feature: Paper 엔티티 추가, 일대다 관계 설정

---
+++ src/main/java/com/sparklenote/domain/entity/Paper.java
@@ -0,0 +1,32 @@
+package com.sparklenote.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paper extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int paperId;
+
+    private String content;
+
+    private String sticker;
+
+    @ManyToOne
+    @JoinColumn(name = "roll_id")
+    private Roll roll;
+
+    @OneToMany(mappedBy = "paper")
+    private List<Sticker> stickers = new ArrayList<>();
+
+}
