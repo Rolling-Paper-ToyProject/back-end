4dbcdd0ec6194a082b47e28daf690b9c1e9b18f1
pangwoo96 <junggw96@gmail.com>
2024-10-07 18:54:04 +0900

    refactor: studentName 칼럼 추가

--- src/main/java/com/sparklenote/domain/entity/Paper.java
+++ src/main/java/com/sparklenote/domain/entity/Paper.java
@@ -1,5 +1,6 @@
 package com.sparklenote.domain.entity;
 
+import com.sparklenote.paper.dto.request.PaperRequestDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,11 +19,14 @@ public class Paper extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "paper_id")
     private Long paperId;
 
     private String content;
 
     private String sticker;
+
+    private String studentName; // 학생 이름 필드 추가
 
     @ManyToOne
     @JoinColumn(name = "roll_id")
@@ -30,5 +34,17 @@ public class Paper extends BaseTimeEntity{
 
     @OneToMany(mappedBy = "paper")
     private List<Sticker> stickers = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student; // 학생에 대한 외래 키 (User)
+
+    public static Paper fromDtoToPaper(PaperRequestDTO paperRequestDTO, String studentName) {
+        Paper paper = Paper.builder()
+                .content(paperRequestDTO.getContent())
+                .studentName(studentName) // User의 name을 studentName으로 설정
+                .build();
+        return paper;
+    }
 
 }
