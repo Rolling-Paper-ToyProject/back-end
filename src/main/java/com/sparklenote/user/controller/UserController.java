4d9bb84caa652827766c73a08ac9bf805906e680
Soo186 <rlagustn9797@gmail.com>
2024-10-07 18:48:26 +0900

    feat : UserController api 추가

--- src/main/java/com/sparklenote/user/controller/UserController.java
+++ src/main/java/com/sparklenote/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sparklenote.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @RestController
 public class UserController {
+
+    @GetMapping("/my")
+    public String myAPI() {
+
+        return "my route";
+    }
+
+    @GetMapping("/")
+    public String mainAPI() {
+
+        return "main route";
+    }
 }
