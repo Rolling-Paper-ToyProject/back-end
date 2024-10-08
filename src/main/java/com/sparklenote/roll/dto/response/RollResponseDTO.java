46d801720f76f33a9a83d50fc2f9f34c5ba94c55
Soo186 <rlagustn9797@gmail.com>
2024-10-08 17:13:11 +0900

    feat : RollResponseDTO 추가

--- src/main/java/com/sparklenote/roll/dto/response/RollResponseDTO.java
+++ src/main/java/com/sparklenote/roll/dto/response/RollResponseDTO.java
@@ -1,6 +1,7 @@
 package com.sparklenote.roll.dto.response;
 
 
+import com.sparklenote.domain.entity.Roll;
 import lombok.*;
 
 
@@ -11,8 +12,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RollResponseDTO {
-    private String rollName;
-    private String rollCode;
-    private String url;
-    private Long userId;
+    private String rollName;  // 사용자가 입력한 학급 이름
+    private int classCode;    // 서버가 생성한 학급 코드 (변경)
+    private String url;       // 서버가 생성한 URL
+    private Long userId;      // 사용자 ID
+
+    // DTO 변환 메서드
+    public static RollResponseDTO fromRoll(Roll roll, Long userId) {
+        return RollResponseDTO.builder()
+                .rollName(roll.getRollName())
+                .classCode(roll.getClassCode()) // 변경된 변수명 사용
+                .url(roll.getUrl())
+                .userId(userId)
+                .build();
+    }
 }
