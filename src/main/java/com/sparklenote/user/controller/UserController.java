e4e584223eb7a09e8e9475c796569e13ffb899d1
pangwoo96 <junggw96@gmail.com>
2024-10-09 18:38:05 +0900

    feature: /logout API 추가

--- src/main/java/com/sparklenote/user/controller/UserController.java
+++ src/main/java/com/sparklenote/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sparklenote.user.controller;
 
 import com.sparklenote.common.response.SnResponse;
 import com.sparklenote.user.dto.request.TokenRequestDTO;
+import com.sparklenote.user.dto.response.BlacklistResponseDTO;
 import com.sparklenote.user.dto.response.TokenResponseDTO;
 import com.sparklenote.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -36,5 +37,14 @@ public class UserController {
         response.addCookie(userService.createCookie("Authorization", newAccessToken.getAccessToken()));
         return ResponseEntity.status(SUCCESS.getStatus())
                 .body(new SnResponse<>(SUCCESS, newAccessToken));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<SnResponse<BlacklistResponseDTO>> logout(@RequestBody TokenRequestDTO tokenRequestDTO) {
+        String refreshToken = tokenRequestDTO.getRefreshToken();
+        // 로그아웃 시 리프레시 토큰을 redis 블랙리스트에 추가
+        userService.handleLogout(refreshToken);
+        return ResponseEntity.status(SUCCESS.getStatus())
+                .body(new SnResponse<>(SUCCESS, new BlacklistResponseDTO(refreshToken)));
     }
 }
