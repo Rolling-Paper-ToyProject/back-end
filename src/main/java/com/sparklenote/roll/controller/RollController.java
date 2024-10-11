563b3666e61ab3c50d6d94378ed3ecb3d917b1d4
Soo186 <rlagustn9797@gmail.com>
2024-10-11 18:59:52 +0900

    feat: Roll 입장 API 응답 DTO 변경 및 코드 정리

--- src/main/java/com/sparklenote/roll/controller/RollController.java
+++ src/main/java/com/sparklenote/roll/controller/RollController.java
@@ -3,6 +3,7 @@ package com.sparklenote.roll.controller;
 import com.sparklenote.common.response.SnResponse;
 import com.sparklenote.roll.dto.request.RollCreateRequestDto;
 import com.sparklenote.roll.dto.request.RollJoinRequestDto;
+import com.sparklenote.roll.dto.response.RollJoinResponseDto;
 import com.sparklenote.roll.dto.response.RollResponseDTO;
 import com.sparklenote.roll.service.RollService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -47,9 +48,10 @@ public class RollController {
     }
     @PostMapping(value = "/join/{url}", produces = "application/json;charset=UTF-8")
     @Operation(summary = "학생이 Roll에 입장", description = "주어진 URL과 학급 코드를 사용하여 Roll에 입장합니다.")
-    public ResponseEntity<SnResponse<RollJoinRequestDto>> joinRoll(@PathVariable String url, @Valid @RequestBody RollJoinRequestDto joinRequestDto) {
-        RollJoinRequestDto responseDto = rollService.joinRoll(url, joinRequestDto);
+    public ResponseEntity<SnResponse<RollJoinResponseDto>> joinRoll(@PathVariable String url, @Valid @RequestBody RollJoinRequestDto joinRequestDto) {
+        RollJoinResponseDto responseDto = rollService.joinRoll(url, joinRequestDto);
         return ResponseEntity.ok(new SnResponse<>(SUCCESS, responseDto));
     }
+
 }
 
