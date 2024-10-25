b356ad9f2c9afa601bd55e5357027382ffa89616
pangwoo96 <junggw96@gmail.com>
2024-10-26 00:57:16 +0900

    feat : Error 글로벌로 처리
    - BaseErrorCode : 자주 사용하는 메소드 (getter) 정의
    - ErrorResponse : 에러 관련 응답 형식
    - GlobalErrorCode : 500 에러 설정
    - GlobalExceptionHandler : 각 예외 클래스 핸들러 구현
    - PaperErrorCode / PaperException : Paper에서 발생하는 에러코드, 실제 Paper 예외 호출 클래스
    - RollErrorCode / RollException : Roll에서 발생하는 에러코드, 실제 Roll 예외 호출 클래스
    - UserErrorCode / UserException : User에서 발생하는 에러코드, 실제 User 예외 호출

---
+++ src/main/java/com/sparklenote/common/error/code/PaperErrorCode.java
@@ -0,0 +1,26 @@
+package com.sparklenote.common.error.code;
+
+import com.sparklenote.common.error.response.ErrorResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum PaperErrorCode implements BaseErrorCode{
+    PAPER_NOT_FOUND(404, "Paper를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final int errorCode;
+    private final String errorMessage;
+    private final HttpStatus status;
+
+    PaperErrorCode(int errorCode, String message, HttpStatus status) {
+        this.errorCode = errorCode;
+        this.errorMessage = message;
+        this.status = status;
+    }
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return new ErrorResponse(this.errorCode, this.errorMessage);
+    }
+}
+
