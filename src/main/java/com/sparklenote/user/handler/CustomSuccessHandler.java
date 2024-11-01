e29ab3e131a1fad7c7b87be0265b9f402923d533
Soo186 <rlagustn9797@gmail.com>
2024-11-01 19:01:29 +0900

    refactor : redirect 경로 삭제

--- src/main/java/com/sparklenote/user/handler/CustomSuccessHandler.java
+++ src/main/java/com/sparklenote/user/handler/CustomSuccessHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -15,30 +16,31 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j  // 롬복 로그 어노테이션 추가
 @RequiredArgsConstructor
 @Component
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JWTUtil jwtUtil;
 
-    @Value("${jwt.accessExpiration}") // 30분
+    @Value("${jwt.accessExpiration}")
     private Long accessTokenExpiration;
 
-    @Value("${jwt.refreshExpiration}") // 1일
+    @Value("${jwt.refreshExpiration}")
     private Long refreshTokenExpiration;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        //OAuth2User
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         String username = customUserDetails.getUsername();
+
 
         String accessToken = jwtUtil.createAccessToken(username, Role.TEACHER, accessTokenExpiration);
         String refreshToken = jwtUtil.createRefreshToken(username, refreshTokenExpiration);
 
-        response.setHeader("Authorization", "Bearer " + accessToken);
+
+        response.setHeader("Authorization", "Bearer "+ accessToken);
         response.setHeader("RefreshToken", refreshToken);
-        response.sendRedirect("http://localhost:3000/oauth/callback");
     }
 }
