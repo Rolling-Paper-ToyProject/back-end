2ec70e645c69e8745fada189ff954ae2d9bef6cc
Soo186 <161421422+Soo186@users.noreply.github.com>
2024-12-05 01:13:59 +0900

    Fix/ pinnumber error - 사용자가 0으로 시작하는 비밀번호를 칠 수 있게끔 수정  (#125)
    * fix : pinNumber를 String으로 처리
    
    - 사용자가 0으로 비밀번호를 사용하고 싶어할 때 오류 발생 + 정확한 오류 메시지가 나오지 않음
    
    * fix : pinNumber를 String으로 처리

--- src/main/java/com/sparklenote/domain/repository/StudentRepository.java
+++ src/main/java/com/sparklenote/domain/repository/StudentRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     
     // 클래스 코드, 이름, 핀번호로 학생을 찾는 메소드
-    Optional<Student> findByNameAndPinNumberAndRollId(String name, int pinNumber,Long id);
+    Optional<Student> findByNameAndPinNumberAndRollId(String name, String pinNumber,Long id);
     Optional<Student> findByName(String username);
 }
 
